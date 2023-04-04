4bfed535ac65f18ca3d6c145576aff161b000971
kayceewong <kayceewonghk@gmail.com>
2023-04-04 16:30:44 +0800

    main page layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
 export default function Home() {
   return (
-    <h1 className="text-center my-5">Home Page | Hello World</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - VGDB</title>
+      </Head>
+      Home Page | Hello World
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+
+    </div>
   )
 }
